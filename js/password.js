86c851cf9da0ca565737db8cf491b02db3fd3d2d
Jackson Luis Fagundes Schirigatti <jacksonschirigatti.fag@gmail.com>
2022-09-15 17:55:04 -0300

    update cache

--- js/password.js
+++ js/password.js
@@ -3,9 +3,15 @@ let input = document.querySelector('#key')
 let button = document.querySelector('#confirm')
 .addEventListener('click', () => {
     if (input.value == "schirigatti") {
-        console.log("Foi");
-        window.location.href = "pages/names.html"
+        localStorage.setItem("acess", true);
     } else {
         console.log("Invalid Password");
     }
 })
+
+setInterval(() => {
+    
+    if (localStorage.getItem("acess") == "true") {
+        window.location.href = "pages/names.html"
+    }
+}, 1000);
